6344bc0ace53a53d8616d62910d280937e5eae49
Mark Reed <markdreed512@gmail.com>
2021-02-18 12:46:15 -0600

    react app config

--- bugtracker/bugtracker/settings.py
+++ bugtracker/bugtracker/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'tickets',
     'rest_framework',
+    'frontend'
 ]
 
 MIDDLEWARE = [
--- bugtracker/bugtracker/urls.py
+++ bugtracker/bugtracker/urls.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('tickets.urls')),
+    path('', include('frontend.urls')),
+    path('', include('tickets.urls'))
 ]
